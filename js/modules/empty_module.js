2c84d5126be961b3318238a9eddc9d67fcd0f28a
Aaron Meyers <aaron@freqout-2.local>
2013-08-29 18:10:24 -0400

    added empty module

---
+++ js/modules/empty_module.js
@@ -0,0 +1,26 @@
+EmptyModule = function( audio, scene, guiFolder ) {
+
+	// Standard Module Vars
+	this.audio = audio;
+	this.scene = scene;
+	this.node = new THREE.Object3D();
+	this.gui = guiFolder;
+	this.init();
+}
+
+EmptyModule.prototype.on = function() {
+	this.scene.add( this.node );
+}
+
+EmptyModule.prototype.off = function() {
+	this.scene.remove( this.node );
+}
+
+EmptyModule.prototype.init = function() {
+	this.empty = "i'm empty!";
+	this.gui.add( this, 'empty' );
+}
+
+EmptyModule.prototype.update = function() {
+
+}
